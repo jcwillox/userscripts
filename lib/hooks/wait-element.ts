f2b5221b63568df8ce771f9c62a2643aa1249768
Josh Willox <joshwillox@gmail.com>
2023-05-27 15:43:22 +1000

    feat: improve `useSelector` logic

--- lib/hooks/wait-element.ts
+++ lib/hooks/wait-element.ts
@@ -1,31 +1,61 @@
-// watch selector for changes, fires callback every time the element is added
-// call observer.disconnect() to stop watching
+export interface UseSelectorOptions {
+  root?: Element;
+  event?: "added" | "removed";
+}
+
+//! watch selector for changes, fires callback every time the element is added
+//! call observer.disconnect() to stop watching
 export function useSelector<T extends Element = Element>(
-  selector: string,
+  selector: Node | string,
   callback: (element: T) => void,
-  root: Node = document.body
+  options: UseSelectorOptions = {}
 ) {
-  for (const el of document.querySelectorAll<T>(selector)) {
-    callback(el);
+  const opts: Required<UseSelectorOptions> = {
+    root: options.root || document.body,
+    event: options.event || "added",
+  };
+
+  if (opts.event == "added") {
+    if (typeof selector == "string") {
+      for (const el of document.querySelectorAll<T>(selector)) {
+        callback(el);
+      }
+    } else {
+      callback(selector as T);
+    }
   }
-  const observer = new MutationObserver(mutations => {
+
+  const observer = new MutationObserver((mutations) => {
     for (const mutation of mutations) {
-      for (const node of mutation.addedNodes) {
+      const nodes =
+        opts.event == "added" ? mutation.addedNodes : mutation.removedNodes;
+      const matches =
+        typeof selector == "string" && opts.root.querySelectorAll<T>(selector);
+      for (const node of nodes) {
         if (node.nodeType === Node.ELEMENT_NODE) {
           const el = node as Element;
-          if (el.matches(selector)) {
+          if (typeof selector == "string") {
+            if (matches) {
+              for (const match of matches) {
+                if (match == el) {
+                  callback(match);
+                  break;
+                }
+              }
+            }
+          } else if (node == selector) {
             callback(el as T);
           }
         }
       }
     }
   });
-  observer.observe(root, { childList: true, subtree: true });
+  observer.observe(opts.root, { childList: true, subtree: true });
   return observer;
 }
 
 export interface WaitElementOptions {
-  root?: Node;
+  root?: Element;
   timeout?: number;
 }
 
@@ -34,18 +64,18 @@ export function useWaitElement<T extends Element = Element>(
   options: WaitElementOptions = {}
 ): Promise<T> {
   return new Promise((resolve, reject) => {
-    // check if element already exists
+    //! check if element already exists
     const el = document.querySelector<T>(selector);
     if (el) return resolve(el);
 
-    // wait for element to be added
+    //! wait for element to be added
     const observer = useSelector<T>(
       selector,
-      element => {
+      (element) => {
         observer.disconnect();
         resolve(element);
       },
-      options.root
+      { root: options.root }
     );
 
     if (options.timeout === undefined || options.timeout > 0) {
