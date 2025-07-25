254b23eb3359b5ac55d50d4a2afb669d57e654b1
Josh Willox <joshwillox@gmail.com>
2025-07-25 19:35:00 +1000

    chore: update unocss config and tsconfig

--- uno.config.ts
+++ uno.config.ts
@@ -9,11 +9,13 @@ import {
 export default defineConfig({
   include: ["src/**/*.ts"],
   presets: [
-    presetUno(),
-    presetIcons({
-      cdn: "https://esm.sh/",
-      warn: true,
-    }),
+    presetUno({ prefix: "un-" }),
+    presetIcons({ cdn: "https://esm.sh/" }),
   ],
+  shortcuts: {
+    "un-btn":
+      "un-(rounded-full inline-flex items-center justify-center relative text-inherit overflow-hidden) before:un-(pointer-events-none absolute inset-0 bg-current opacity-0 transition-opacity content-['']) hover:before:un-opacity-10",
+    "un-btn-small": "un-(h-8 w-8)",
+  },
   transformers: [transformerVariantGroup(), transformerCompileClass()],
 });
