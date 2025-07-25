1efece05b38c64ee0800f136572d7876dc4c7675
Josh Willox <joshwillox@gmail.com>
2025-07-25 19:41:03 +1000

    feat: update open in youtube to use new utils

--- src/userscripts/open-in-youtube/main.ts
+++ src/userscripts/open-in-youtube/main.ts
@@ -1,36 +1,48 @@
-import { mdiYoutube } from "@mdi/js";
 import { useSelector } from "@/hooks";
+import { createElement } from "@/utils";
 
-function createButton(url: () => string, css: Record<string, unknown>) {
-  const button = createIconButton(mdiYoutube, (e) => {
-    // ignore if not left or middle click
-    if (e.button > 1) return;
-    e.preventDefault();
-    e.stopPropagation();
-    // check if middle mouse button is pressed
-    if (e.button == 1) {
-      GM_openInTab(url());
-    } else if (e.button == 0) {
-      location.href = url();
-    }
+declare global {
+  interface HTMLElementTagNameMap {
+    "ytd-reel-video-renderer": HTMLElement & {
+      data?: { command?: { reelWatchEndpoint?: { videoId?: string } } };
+    };
+  }
+}
+
+GM_addStyle(`@unocss-placeholder
+`);
+
+function createButton(url: () => string, className?: string) {
+  return createElement("button", {
+    className: "un-btn un-btn-small",
+    title: "Open in YouTube",
+    onclick: (e) => {
+      // ignore if not left or middle click
+      if (e.button > 1) return;
+      e.preventDefault();
+      e.stopPropagation();
+      // check if middle mouse button is pressed
+      if (e.button == 1) {
+        GM_openInTab(url());
+      } else if (e.button == 0) {
+        location.href = url();
+      }
+    },
+    children: createElement("span", {
+      className: `i-mdi-youtube un-(h-[18px] w-[18px]) ${className}`,
+    }),
   });
-  button.title = "Open in YouTube";
-  Object.assign(button.style, css);
-  return button;
 }
 
 if (location.hostname === "music.youtube.com") {
-  const el = document.querySelector<HTMLDivElement>(
+  const el = document.querySelector(
     "#player div.ytmusic-player.top-row-buttons"
   );
   if (el) {
     el.prepend(
-      createButton(
-        () => {
-          return location.href.replace("music.", "www.");
-        },
-        { padding: "8px" }
-      )
+      createButton(() => {
+        return location.href.replace("music.", "www.");
+      }, "un-m-2")
     );
   }
 } else {
@@ -38,19 +50,15 @@ if (location.hostname === "music.youtube.com") {
     const controlsEl = element.querySelector("ytd-shorts-player-controls");
     if (controlsEl) {
       controlsEl.insertBefore(
-        createButton(
-          () => {
-            const url = new URL(location.href);
-            url.pathname = "/watch";
-            url.searchParams.set(
-              "v",
-              // eslint-disable-next-line @typescript-eslint/no-explicit-any
-              (element as any).data.command.reelWatchEndpoint.videoId
-            );
-            return url.href;
-          },
-          { color: "white" }
-        ),
+        createButton(() => {
+          const url = new URL(location.href);
+          url.pathname = "/watch";
+          url.searchParams.set(
+            "v",
+            element.data?.command?.reelWatchEndpoint?.videoId || ""
+          );
+          return url.href;
+        }, "un-text-white"),
         controlsEl.lastElementChild
       );
     }
