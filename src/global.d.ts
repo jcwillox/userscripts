94fb833dff30c77c64d5a9de3d86888b524931c4
Josh Willox <joshwillox@gmail.com>
2025-07-25 19:31:19 +1000

    fix: add global reference types

--- src/global.d.ts
+++ src/global.d.ts
@@ -1,4 +1,6 @@
 /// <reference types="vite/client" />
+/// <reference types="typed-query-selector/strict" />
+/// <reference types="@types/tampermonkey" />
 
 import { HLJSApi } from "highlight.js";
 
