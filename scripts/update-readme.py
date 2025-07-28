d28343a47b77d74edae81e0a672060a966d7975c
Josh Willox <joshwillox@gmail.com>
2025-07-28 17:59:39 +1000

    chore: update readme generation script

--- scripts/update-readme.py
+++ scripts/update-readme.py
@@ -1,7 +1,19 @@
+# /// script
+# dependencies = [
+#   "md-template[full]>=0.2.1",
+# ]
+# ///
+
+
 try:
     from mdtemplate.table.presets.userscripts import UserscriptsTableTemplate
 except ImportError:
     raise ImportError("Please install md-template: pip install md-template[full]")
 
+
+class TableTemplate(UserscriptsTableTemplate):
+    files = "src/userscripts/*/meta.yaml"
+
+
 if __name__ == "__main__":
-    UserscriptsTableTemplate().parse_args().render()
+    TableTemplate().parse_args().render()
