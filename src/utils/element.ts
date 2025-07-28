c0c9c78b0d4c6b579439f852e5ecd7fee21f95cb
Josh Willox <joshwillox@gmail.com>
2025-07-28 18:02:00 +1000

    feat: support css text on create element

--- src/utils/element.ts
+++ src/utils/element.ts
@@ -9,11 +9,12 @@ type ElementOptions<T extends keyof HTMLElementTagNameMap> = Omit<
 
 export function createElement<T extends keyof HTMLElementTagNameMap>(
   tag: T,
-  options: ElementOptions<T>
+  { style, ...options }: Omit<ElementOptions<T>, "style"> & { style?: string }
 ) {
   const el = document.createElement(tag);
   const { children, ...attributes } = options;
   Object.assign(el, attributes);
+  if (style) el.style.cssText = style;
   children && el.append(...coerceArray(children));
   return el;
 }
